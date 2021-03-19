609d4a8c90c7bafd9d529a8653955c57f8a06bda
matcur <ru.far2014@yandex.ru>
2021-03-19 11:41:42 +0300

    Изменил название файла Menu на Navbar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { AuthLinks } from "./AuthLinks"
 import { Logo } from "./Logo"
-import { Navbar } from "./Menu"
+import { Navbar } from "./Navbar"
 
 export const Header: React.FC = () => {
   return (
